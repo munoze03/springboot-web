4fb2333dd7ea7016fe6143f6e324402583f90e1e
Enrique <munoze03@gmail.com>
2025-07-18 15:42:00 +0200

    Api Rest enviar Json petición POST

--- src/main/java/com/enrique/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/enrique/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.enrique.curso.springboot.webapp.springboot_web.models.User;
 import com.enrique.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 @RestController
@@ -30,5 +33,13 @@ public class PathVariableController {
         json.put("id", id);
 
         return json;
+    }
+
+    @PostMapping("/create")
+    public User create (@RequestBody User user){
+        //Hacer algo con el usuario, ejemplo save en la bbdd
+        user.setName(user.getName().toUpperCase());
+
+        return user;
     }
 }

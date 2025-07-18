789a78a7f9a827a0aa529d3bcd46893b738b8961
Enrique <munoze03@gmail.com>
2025-07-18 15:27:39 +0200

    @PathVariable Multiple

--- src/main/java/com/enrique/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/enrique/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -1,5 +1,8 @@
 package com.enrique.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,5 +20,15 @@ public class PathVariableController {
         ParamDto param = new ParamDto();
         param.setMessage(message);
         return param;
+    }
+
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id){
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+
+        return json;
     }
 }

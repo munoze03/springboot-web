43784bd671dc61ebb5dcf6601358ef5571c22bfc
Enrique <munoze03@gmail.com>
2025-07-19 15:34:55 +0200

    Agregando otros archivos properties personalizados para las configuraciones

---
+++ src/main/java/com/enrique/curso/springboot/webapp/springboot_web/ValuesConfig.java
@@ -0,0 +1,14 @@
+package com.enrique.curso.springboot.webapp.springboot_web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+		@PropertySource("classpath:values.properties")
+})
+
+public class ValuesConfig {
+
+}
